760716023b652b51cf06394b9aca09669e9253a9
doulabi <dyhaeoulabi@outlook.com>
2020-03-27 18:36:08 -0500

    create the user mdodel and then modified the state and the category with the one to many relationship with product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_233306) do
+ActiveRecord::Schema.define(version: 2020_03_27_233428) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,15 @@ ActiveRecord::Schema.define(version: 2020_03_27_233306) do
 
   create_table "states", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "alias"
+    t.boolean "is_admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
