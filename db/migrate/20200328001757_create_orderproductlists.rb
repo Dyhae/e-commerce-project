1eb8979ed6ccbe8773a11c7c21e4bc1962b785c9
doulabi <dyhaeoulabi@outlook.com>
2020-03-27 19:18:20 -0500

    modified again the orderproductlist model

--- db/migrate/20200328001757_create_orderproductlists.rb
+++ db/migrate/20200328001757_create_orderproductlists.rb
@@ -1,10 +1,6 @@
 class CreateOrderproductlists < ActiveRecord::Migration[6.0]
   def change
     create_table :orderproductlists do |t|
-      t.string :rails
-      t.string :g
-      t.string :model
-      t.string :orderproductlist
       t.float :price
       t.integer :quantity
       t.references :oder, null: false, foreign_key: true
