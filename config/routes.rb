4b13467ff4ff2a20593faf7233740ba5d69c45b5
doulabi <dyhaeoulabi@outlook.com>
2020-03-28 15:58:17 -0500

    Add more to the seed file by creating more states

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'prducts/index'
-  get 'prducts/show'
+  get 'states/index'
+  get 'states/show'
+  get 'categories/index'
+  get 'categories/show'
+  # get 'products/index'
+  # get 'products/show'
+  # get 'prducts/index'
+  # get 'prducts/show'
+
+  root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,40 +10,42 @@
 require 'faker'
 require 'csv'
 
-Product.destroy_all
-State.destroy_all
-Category.destroy_all
-Status.destroy_all
-Province.destroy_all
-User.destroy_all
-Order.destroy_all
-Address.destroy_all
-Addresslist.destroy_all
-Orderproductlist.destroy_all
+# Product.destroy_all
+# State.destroy_all
+# Category.destroy_all
+# Status.destroy_all
+# Province.destroy_all
+# User.destroy_all
+# Order.destroy_all
+# Address.destroy_all
+# Addresslist.destroy_all
+# Orderproductlist.destroy_all
 
-state = State.create(name: 'New')
-state.save
-
-# myArray = ["red", "blue", "white", "black", "yellow", "white and black"]
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'card_csvfile.csv'))
-csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
-csv.each do |row|
-  puts row.to_hash
-  category = Category.find_or_create_by(name: row['Make'])
-  category.save
-  price = rand(1000.00..120_000.00)
-  product = Product.find_or_create_by(model: row['Model'])
-  product.color = Faker::Vehicle.color
-  product.transmission = Faker::Vehicle.transmission
-  product.car_description = Faker::Vehicle.standard_specs[0]
-  product.car_option = Faker::Vehicle.car_options[0]
-  product.price = price
-  product.sale_percentage = 0.0
-  product.quantity_in_stock = 1
-  product.state = state
-  product.category = category
-  product.save
+myArray = ['New', 'On Sale']
+myArray.each do |p|
+  state = State.find_or_create_by(name: p)
+  state.save
 end
+
+# csv_text = File.read(Rails.root.join('lib', 'seeds', 'card_csvfile.csv'))
+# csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+# csv.each do |row|
+#   puts row.to_hash
+#   category = Category.find_or_create_by(name: row['Make'])
+#   category.save
+#   price = rand(1000.00..120_000.00)
+#   product = Product.find_or_create_by(model: row['Model'])
+#   product.color = Faker::Vehicle.color
+#   product.transmission = Faker::Vehicle.transmission
+#   product.car_description = Faker::Vehicle.standard_specs[0]
+#   product.car_option = Faker::Vehicle.car_options[0]
+#   product.price = price
+#   product.sale_percentage = 0.0
+#   product.quantity_in_stock = 1
+#   product.state = state
+#   product.category = category
+#   product.save
+# end
 
 # 7.times do
 #   category = Category.find_or_create_by(name: Faker::Vehicle.make)
