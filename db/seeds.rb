84e24426c5ddbad2c556d8449638240185584beb
doulabi <dyhaeoulabi@outlook.com>
2020-03-28 15:29:40 -0500

    Populated the seed script with csv file and faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,60 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require 'csv'
 
+Product.destroy_all
 State.destroy_all
 Category.destroy_all
 Status.destroy_all
 Province.destroy_all
 User.destroy_all
-Product.destroy_all
 Order.destroy_all
 Address.destroy_all
 Addresslist.destroy_all
 Orderproductlist.destroy_all
 
-6.times do
-  category = Category.find_or_create_by(name: Faker::Vehicle.make)
+state = State.create(name: 'New')
+state.save
+
+# myArray = ["red", "blue", "white", "black", "yellow", "white and black"]
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'card_csvfile.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+csv.each do |row|
+  puts row.to_hash
+  category = Category.find_or_create_by(name: row['Make'])
   category.save
+  price = rand(1000.00..120_000.00)
+  product = Product.find_or_create_by(model: row['Model'])
+  product.color = Faker::Vehicle.color
+  product.transmission = Faker::Vehicle.transmission
+  product.car_description = Faker::Vehicle.standard_specs[0]
+  product.car_option = Faker::Vehicle.car_options[0]
+  product.price = price
+  product.sale_percentage = 0.0
+  product.quantity_in_stock = 1
+  product.state = state
+  product.category = category
+  product.save
 end
+
+# 7.times do
+#   category = Category.find_or_create_by(name: Faker::Vehicle.make)
+#   category.save
+#   20.times do
+#     price = rand(1000.00..120_000.00)
+#     product = Product.find_or_create_by(model: Faker::Vehicle.model(make_of_model: category.name))
+#     product.color = Faker::Vehicle.color
+#     product.transmission = Faker::Vehicle.transmission
+#     product.car_description = Faker::Vehicle.standard_specs[0]
+#     product.car_option = Faker::Vehicle.car_options[0]
+#     product.price = price
+#     product.sale_percentage = 0.0
+#     product.quantity_in_stock = 1
+#     product.state = state
+#     product.category = category
+#     product.save
+#   end
+# rescue StandardError => e
+#   puts e.message
+# end
